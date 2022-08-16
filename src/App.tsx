6079b43de9042025f39a12c521799da721d98588
Pete <Peter.Schnabel@starshiphsa.com>
2022-08-16 09:10:14 -0400

    added option to handle multiple dogs

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Fragment } from 'react';
+
 import { data } from './data';
 
 const buildExpiredDate = () => {
@@ -79,68 +81,72 @@ const getStamp = () => (
   </svg>
 );
 
-const App = () => {
-  const { dogName, dogImage, handlers, phone, city, state } = data;
-
-  return (
-    <div className="container max-w-3xl mx-auto my-4 border-red-600 border-2 p-1 rounded-lg">
-      <div className="grid grid-cols-3 bg-red-600 rounded-t-md p-4">
-        <div className="col-start-1 col-end-2 text-center text-white font-bold uppercase text-4xl">
-          <p>service dog</p>
-        </div>
-        {/* <div className="col-end-4 col-span-1 text-center text-white font-bold">
+const App = () => (
+  <>
+    {data.map(({ dogName, dogImage, handlers, phone, city, state }) => (
+      <Fragment key={dogName}>
+        <div className="container max-w-3xl mx-auto my-4 border-red-600 border-2 p-1 rounded-lg">
+          <div className="grid grid-cols-3 bg-red-600 rounded-t-md p-4">
+            <div className="col-start-1 col-end-2 text-center text-white font-bold uppercase text-4xl">
+              <p>service dog</p>
+            </div>
+            {/* <div className="col-end-4 col-span-1 text-center text-white font-bold">
           <p>its real</p>
         </div> */}
-      </div>
-
-      <div className="grid grid-cols-3 py-2 gap-4">
-        <div className="col-start-1 col-end-2">
-          <img
-            src={dogImage}
-            alt={`${dogName.toUpperCase()} the service dog`}
-            className="border-black border-8 h-50"
-          />
-        </div>
-        <div className="col-end-4 col-span-2 flex flex-col justify-around relative">
-          <div className="absolute top-0 right-5 opacity-70 -z-10">
-            {getStamp()}
           </div>
 
-          <div>
-            <p className="text-red-600 font-bold uppercase text-3xl">
-              {dogName}
-            </p>
-            <p className="italic text-xl capitalize">Service Dog</p>
-            <p className="italic">SD#: {Date.now()}</p>
-            <p className="italic">Expires: {buildExpiredDate()}</p>
-          </div>
-
-          <div className="c-add-top-bottom-spacing-small-screen">
-            <div className="text-red-600 font-bold capitalize text-3xl">
-              {handlers.map((handler) => (
-                <p key={handler}>{handler}</p>
-              ))}
+          <div className="grid grid-cols-3 py-2 gap-4">
+            <div className="col-start-1 col-end-2">
+              <img
+                src={dogImage}
+                alt={`${dogName.toUpperCase()} the service dog`}
+                className="border-black border-8 h-50"
+              />
             </div>
-            <p className="italic text-xl capitalize">
-              handler{handlers.length > 1 ? 's' : ''}
-            </p>
-            <p className="italic">
-              <span className="capitalize">{city}</span>,{' '}
-              <span className="uppercase">{state}</span>
-            </p>
+            <div className="col-end-4 col-span-2 flex flex-col justify-around relative">
+              <div className="absolute top-0 right-5 opacity-70 -z-10">
+                {getStamp()}
+              </div>
+
+              <div>
+                <p className="text-red-600 font-bold uppercase text-3xl">
+                  {dogName}
+                </p>
+                <p className="italic text-xl capitalize">Service Dog</p>
+                <p className="italic">SD#: {Date.now()}</p>
+                <p className="italic">Expires: {buildExpiredDate()}</p>
+              </div>
+
+              <div className="c-add-top-bottom-spacing-small-screen">
+                <div className="text-red-600 font-bold capitalize text-3xl">
+                  {handlers.map((handler) => (
+                    <p key={handler}>{handler}</p>
+                  ))}
+                </div>
+                <p className="italic text-xl capitalize">
+                  handler{handlers.length > 1 ? 's' : ''}
+                </p>
+                <p className="italic">
+                  <span className="capitalize">{city}</span>,{' '}
+                  <span className="uppercase">{state}</span>
+                </p>
+              </div>
+
+              <p>If separated from handler please call: {phone}</p>
+            </div>
           </div>
 
-          <p>If separated from handler please call: {phone}</p>
+          <div className="bg-red-600 rounded-b-md p-4">
+            <p className="text-white text-center text-lg font-bold">
+              In case of accident, do not separate service dog from handler
+            </p>
+          </div>
         </div>
-      </div>
 
-      <div className="bg-red-600 rounded-b-md p-4">
-        <p className="text-white text-center text-lg font-bold">
-          In case of accident, do not separate service dog from handler
-        </p>
-      </div>
-    </div>
-  );
-};
+        <div className="mb-14 last:mb-0" />
+      </Fragment>
+    ))}
+  </>
+);
 
 export default App;
--- src/data.ts
+++ src/data.ts
@@ -1,10 +1,12 @@
 import dixie from './images/dixie.jpg';
 
-export const data = {
-  dogName: 'dixie',
-  dogImage: dixie,
-  handlers: ['pete plays', 'mary marvs'],
-  city: 'charleston',
-  state: 'sc',
-  phone: '3213457890',
-};
+export const data = [
+  {
+    dogName: 'dixie',
+    dogImage: dixie,
+    handlers: ['pete plays', 'mary marvs'],
+    city: 'charleston',
+    state: 'sc',
+    phone: '3213457890',
+  },
+];
